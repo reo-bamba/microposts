313f46898b1f9c9542900a2f5f12976b8ab19cba
reo bamba <12reo08bam@gmail.com>
2020-09-01 04:12:33 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,20 @@
+<header class = "mb-4">
+    <nav class = "navbar navbar-expand-sm navbar-dark bg-dark">
+        {{-- link of top page --}}
+        <a class "navbar-bland" href = "/">MicroPost</a>
+        <btton type = "button" class = "navbar-togglar" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class ="collaps navbar-collapse" id ="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                {{-- link of user's registration --}}
+                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                {{-- link to login page --}}
+                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+            </ul>
+            
+        </div>
+    </nav>
+</header>
